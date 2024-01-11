167b875f601dfc7d3299ffb5ce6eda6197aecc19
wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-11 10:38:27 +0100

    added event listener for my app to running live

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express=require("express")
 const App =express()
+App.listen(3000,(erro)=>{
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log("Server is running on port 3000");
+    }
+})
