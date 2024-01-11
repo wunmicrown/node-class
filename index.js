eb2b29c2502bff31085bc63546f8aabb7ec1a9ff
wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-11 10:53:10 +0100

    redering back to my user using res.send

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express=require("express")
 const App =express()
+
+App.get("/",(req,res)=>{  
+    res.send("Hello World")
+})
+
+
 App.listen(4000,(erro)=>{
     if (erro) {
         console.log(erro);
